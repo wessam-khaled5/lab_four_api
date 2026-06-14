32fd09341c6a0c07f1b527dafc5816d139245e14
-wessam_khaled5 <wessamkhaled53@gmail.com>
2026-06-14 22:20:58 +0300

    Initial commit: complete rails API mode with scaffold and api.txt collection

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[show update destroy]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+    render json: @posts
+  end
+
+  # GET /posts/1
+  def show
+    render json: @post
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    if @post.update(post_params)
+      render json: @post, status: :ok, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+  
+    params.require(:post).permit(:title, :content, :user_id)
+  end
+end
