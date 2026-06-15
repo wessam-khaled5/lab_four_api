ed439ed9acc589c26d7dd047099c447242765279
-wessam_khaled5 <wessamkhaled53@gmail.com>
2026-06-15 17:59:45 +0300

    fix: resolve lint spacing, final newlines, and controller test suites for CI passing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,7 +45,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-  
     params.require(:post).permit(:title, :content, :user_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-  
     params.require(:user).permit(:name, :email)
   end
 end
